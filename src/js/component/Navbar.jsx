9bcb5043f253f091170a25bfece73697b34bc0e8
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-06-27 03:40:06 +0000

    got the dropdown for the navbar working

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="col-10 container-fluid">
+            <div className="container-fluid">
                 <a className="navbar-brand ms-5" href="#">Start Bootstrap</a>
-                <button className="navbar-toggler" type="button" data-toggle="dropdown" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">About</a>
@@ -28,6 +28,7 @@ const Navbar = () => {
         </nav>
     );
 };
+
 export default Navbar;
 
 
